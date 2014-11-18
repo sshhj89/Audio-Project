c04f3d8f48936a7a35bcc5fa869fb0c1bc20914d
Chris Lowe <clowe@luc.edu>
2014-11-18 14:27:58 -0600

    fixes for sd-dev.

--- sd-dev.c
+++ sd-dev.c
@@ -3,9 +3,13 @@
 
 // char *files[3]={"sample1.mp3", "sample2.mp3", "sample3.mp3"};
 int fileCount = 0;
+File dir; 
 
-void countFiles(File dir) {
-	Serial.println("Counting files...")
+char root[] = "/";
+
+countFiles(root);
+void countFiles(const char *path) {
+	Serial.println("Counting files...");
 	while(true) {
 		File file =  dir.openNextFile();
 		if (! file) {
@@ -13,35 +17,36 @@ void countFiles(File dir) {
 			Serial.println("Out of files.");
 		   break;
 		}
+		if (file.isDirectory()) {
+		 	//do nothing
+		} else {
+			fileCount++;
+			Serial.print(fileCount);
+		}
+		file.close();
 	}
-	if (file.isDirectory()) {
-	 	//do nothing
-	} else {
-		fileCount++;
-		Serial.print(fileCount);
-	}
-	file.close();
 }
 
 char *files[fileCount];
 
-void getFiles(File dir) {
+getFiles(root);
+
+void getFiles(const char *path) {
 	int i = 0;
 	while(true) {
-		File file =  dir.openNextFile();
+		File file =  path.openNextFile();
 		if (! file) {
 			// no more files
 			Serial.println("Out of files.");
 		   break;
 		}
-	}
-	if (file.isDirectory()) {
-	 	//do nothing
-	} else {
-		i++;
-		fileCount++;
-		Serial.print(file.name());
-		*files[i] = file.name();
-	}
-	file.close();
+		if (file.isDirectory()) {
+		 	//do nothing
+		} else {
+			i++;
+			fileCount++;
+			Serial.print(file.name());
+			*files[i] = file.name();
+		}
+		file.close();
 }
