b38d05c723d81f8ca036dbc21b99ef97c9cd6212
Chris Lowe <clowe@luc.edu>
2014-11-21 14:39:04 -0600

    Adapted Mellis' and Igoe's project for our purposes. Overhauls much of the SD file checker.

--- sd-dev.c
+++ sd-dev.c
@@ -1,52 +1,103 @@
-/* Read files from an SD card into the files array */
+/*
+SD card basic file example
+
+This example shows how to create and destroy an SD card file 	
+The circuit:
+* SD card attached to SPI bus as follows:
+** MOSI - pin 11
+** MISO - pin 12
+** CLK - pin 13
+** CS - pin 4
+
+created   Nov 2010
+by David A. Mellis
+modified 9 Apr 2012
+by Tom Igoe
+
+This example code is in the public domain.
+	 
+*/
 #include <SD.h>
 
-// char *files[3]={"sample1.mp3", "sample2.mp3", "sample3.mp3"};
-int fileCount = 0;
-File dir; 
+File root;
+int count = 0; 
 
-char root[] = "/";
-
-countFiles(root);
-void countFiles(const char *path) {
-	Serial.println("Counting files...");
-	while(true) {
-		File file =  dir.openNextFile();
-		if (! file) {
-			// no more files
-			Serial.println("Out of files.");
-		   break;
-		}
-		if (file.isDirectory()) {
-		 	//do nothing
-		} else {
-			fileCount++;
-			Serial.print(fileCount);
-		}
-		file.close();
+void setup()
+{
+	// Open serial communications and wait for port to open:
+	Serial.begin(9600);
+	while (!Serial) {
+		; // wait for serial port to connect. Needed for Leonardo only
 	}
+
+	Serial.print("Initializing SD card...");
+	// On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+	// Note that even if it's not used as the CS pin, the hardware SS pin 
+	// (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+	// or the SD library functions will not work. 
+	pinMode(4, OUTPUT);
+
+	if (!SD.begin(10)) {
+		Serial.println("initialization failed!");
+		return;
+	}
+	Serial.println("initialization done.");
+
+	root = SD.open("/");
+
+	countFiles(root);
+	// char *files[count];
+	Serial.println(count);
+	getFiles(root);
+
+	Serial.println("done!");
 }
 
-char *files[fileCount];
+void loop()
+{
+	// nothing happens after setup finishes.
+}
 
-getFiles(root);
+int countFiles(File dir) {
+	while(true) {
 
-void getFiles(const char *path) {
+		File entry =  dir.openNextFile();
+		if (! entry) {
+			// no more files
+			//Serial.println("**nomorefiles**");
+			break;
+		}
+		if (entry.isDirectory()) {
+			//do nothing
+		} else {
+			Serial.print(entry.name());
+			count++; //count the file
+		}
+		entry.close();
+	}
+	return count;
+}
+
+void getFiles(File dir) {
+	char *files[count];
 	int i = 0;
 	while(true) {
-		File file =  path.openNextFile();
-		if (! file) {
-			// no more files
-			Serial.println("Out of files.");
-		   break;
+		File entry = dir.openNextFile();
+		if(!entry){
+			break;
 		}
-		if (file.isDirectory()) {
-		 	//do nothing
-		} else {
+		if(entry.isDirectory()){
+			//do nothing
+		}
+		else {
+			Serial.println(entry.name());
+			files[i] = entry.name(); //write the file name to the array
 			i++;
-			fileCount++;
-			Serial.print(file.name());
-			*files[i] = file.name();
 		}
-		file.close();
+	}
+	char *pointertoarray = &files[0];
+	for(int j= 0; pointertoarray[j] != '\0'; j++)
+	{
+   		Serial.println(*pointertoarray[j]);
+	}
 }
